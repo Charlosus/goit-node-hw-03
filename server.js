4b167daec9925f5fc799a042c8f18915b86724da
Charlosus <145377783+Charlosus@users.noreply.github.com>
2025-06-27 09:49:26 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,22 +7,14 @@ require("dotenv").config();
 
 const { DB_HOST: urlDb } = process.env;
 
-// const urlDb = 'mongodb+srv://sledzkarol93:B7YwUSQ920oDqui5@cluster0.svdecmh.mongodb.net/'
-
-
 const connection = mongoose.connect(urlDb);
 
 const app = express();
 
-app.use(cors()); // Enable CORS
-app.use(express.json()); // Enable parsing JSON body
+app.use(cors()); 
+app.use(express.json()); 
 
-// Your routes should be registered here, for example:
-// const tasksRouter = require('./routes/api/tasks');
-// app.use('/api/tasks', tasksRouter);
 
-// In controller we using next - next is a call back function that
-// process if for example wont succed to add its functionality we add
 
 app.use('/api', apiRouter)
 
